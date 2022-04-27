9346089050726af4562edde9b2f5045e242f5726
JB <qwq140@gmail.com>
2022-04-27 22:38:48 +0900

    결제 결과화면 영수증 추가

--- js/payment_result.js
+++ js/payment_result.js
@@ -1,3 +1,48 @@
-document.onmousedown = function leftClick() {
-    location.href = 'index.html';
-};
+// document.onmousedown = function leftClick() {
+//     location.href = 'index.html';
+// };
+
+const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+const btnOpenPopup = document.querySelector('.btn-open-popup');
+
+btnOpenPopup.addEventListener('click', () => {
+    modal.classList.toggle('show');
+
+    if (modal.classList.contains('show')) {
+        body.style.overflow = 'hidden';
+    }
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.classList.toggle('show');
+
+        if (!modal.classList.contains('show')) {
+            body.style.overflow = 'auto';
+        }
+    }
+});
+
+
+window.onload = () => {
+    let orderMenu = [];
+    orderMenu = JSON.parse(localStorage.getItem("orderMenu"));
+    console.log(orderMenu);
+    let now = new Date();
+    let receiptDate = document.getElementById('receipt_date');
+    console.log(receiptDate);
+    receiptDate.innerText = `${now.getFullYear()}년 ${now.getMonth() + 1}월 ${now.getDate()}일`;
+    let receiptTotal = document.getElementById('receipt_total');
+    let totalPrice = 0;
+    orderMenu.forEach(e => {
+        let html = `<div class="receipt__list-row">
+                    <div class="receipt__item">${e.name} X ${e.count}</div>
+                    <div class="receipt__cost">${(e.price * e.count).toLocaleString()}원</div>
+                </div>`;
+        totalPrice = totalPrice + e.price * e.count;
+        receiptTotal.insertAdjacentHTML('beforebegin', html);
+    });
+    let totalPriceDom = document.getElementById('total_price');
+    totalPriceDom.innerText = `${totalPrice.toLocaleString()}원`
+}
